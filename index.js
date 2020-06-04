1a0832d0f8775079cede62a5fd25430cb1e63f6c
tomMolin <tmolin@leadformance.com>
2020-06-04 09:38:26 +0200

    fix: move index module

---
+++ index.js
@@ -0,0 +1,5 @@
+export { GoogleMap } from "./src/providers/gmaps/Gmaps";
+export { GoogleMapWidget } from "./src/providers/gmaps-widget/GmapsWidget";
+export { Leaflet } from "./src/providers/leaflet/Leaflet";
+export { Mappy } from "./src/providers/mappy/Mappy";
+export { MappyWidget } from "./src/providers/mappy-widget/MappyWidget";
