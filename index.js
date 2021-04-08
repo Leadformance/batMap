dee7f8c7848c97e87b9b2714cdc2a8989928dd37
Marie Rigal <marierigal4@gmail.com>
2021-04-08 13:10:21 +0200

    chore: setup typescript

--- index.js
+++
@@ -1,5 +0,0 @@
-export GoogleMaps from './src/providers/gmaps';
-export GoogleMapsWidget from './src/providers/gmaps-widget';
-export Leaflet from './src/providers/leaflet';
-export Mappy from './src/providers/mappy';
-export MappyWidget from './src/providers/mappy-widget';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './providers/gmaps';
+export * from './providers/gmaps-widget';
+export * from './providers/leaflet';
+export * from './providers/mappy';
+export * from './providers/mappy-widget';
